7b29bccb588edcfdb4bc9d23b5dc7ac32e15882b
JeGa <jens.gansloser@tum.de>
2018-01-20 11:52:12 +0100

    Moved nn init to new file.

--- src/run.m
+++ src/run.m
@@ -36,52 +36,19 @@ y_test = y(n+1:end, :);
 % Define network architecture.
 layers = [dim, 10, 10, 5, classes];
 
-% Number of layers (including last linear layer).
-L = size(layers, 2)-1;
-
-% Weights
-W = cell(1, L);
-% Biases
-b = cell(1, L);
-% Variables for linearities.
-z = cell(1, L);
-% Variables for activations.
-a = cell(1, L-1);
-% Lagrange multipliers.
-lambda = cell(1, L);
-
-% Initialize network weights.
-for l=1:L
-    W{l} = 0.1*randn(layers(l+1), layers(l));
-    b{l} = 0.1*randn(layers(l+1), 1);
-end
-
 % Use sigmoid activation function.
 h = @(t) 1 ./ (1 + exp(-t));
 dh = @(t) h(t) .* (1 - h(t));
 
-% Perform a forward pass to find a feasible initialization of all variables.
-z{1} = W{1}*X_train + b{1} * ones(1, n);
-
-for l=1:L-1
-    % Apply activation function.
-    a{l} = h(z{l});
-
-    % Apply linear mapping.
-    z{l+1} = W{l+1}*a{l} + b{l+1} * ones(1, size(a{l}, 2));
-end
-
-% Initialize Lagrange multipliers for equality constraints.
-for l=1:L
-   lambda{l} = ones(layers(l+1), n); 
-end
+% Initialize variables.
+[W, b, z, a, lambda] = nn(layers, n, h, X_train);
 
 %%
 
 max_iter = 500;
 
 for i=1:max_iter
-    deepsplitting(L, W, a, z, lambda, X_train, y_train, h, dh);
+    deepsplitting(L, W, b, z, a, lambda, X_train, y_train, h, dh);
     disp(['Iteration ', num2str(i)]);
 end
 
