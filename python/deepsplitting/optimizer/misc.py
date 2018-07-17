Fixed ProxProp activation gradient. Changed plot style.

python/deepsplitting/optimizer/misc.py
@@ -1,4 +1,5 @@
 import numpy as np
+from torch.nn import functional as F
 
 
 def prox_cross_entropy(q, tau, y):
@@ -31,8 +32,6 @@ def prox_cross_entropy(q, tau, y):
     return x
 
 
-# TODO: Checked.
-# lambertw_exp(np.array([[-0.576565155510187], [0.586943687333602]]))
 def lambertw_exp(X):
     """
     :param X: Numpy array of shape (c,1).
@@ -101,3 +100,19 @@ def bias_to_end(J, ls):
         from_index = to_index
 
     return np.concatenate(Jw + Jb, axis=1)
+
+
+def grad_relu(x):
+    """
+    :param x: (N, d)
+    :returns: J in matrix form (N, d).
+    """
+    return (x > 0.0).double()
+
+
+def grad_sigmoid(x):
+    """
+    :param x: (N, d)
+    :returns: J in matrix form (N, d).
+    """
+    return F.sigmoid(x).mul(1 - F.sigmoid(x))

python/deepsplitting/optimizer/proxprop.py
@@ -3,6 +3,7 @@ from torch.nn import functional as F
 import scipy
 
 from .base import BaseOptimizer
+import deepsplitting.optimizer.misc as misc
 
 
 class Optimizer(BaseOptimizer):
@@ -10,13 +11,14 @@ class Optimizer(BaseOptimizer):
         super().__init__(net, hyperparams)
 
         if self.net.h is F.relu:
-            self.h_grad = self.h_grad_relu
+            self.h_grad = misc.grad_relu
         elif self.net.h is F.sigmoid:
-            self.h_grad = self.h_grad_sigmoid
+            self.h_grad = misc.grad_sigmoid
         else:
             raise ValueError("Acivation not supported.")
 
     def init_variables(self, inputs, labels):
+        # Initialize non-linearities a and linearities z.
         self.net(inputs)
 
     def init(self, inputs, labels, initializer, parameters=None):
@@ -124,23 +126,9 @@ class Optimizer(BaseOptimizer):
 
         return g
 
-    def h_grad_relu(self, x):
-        """
-        :param x: (N, d)
-        :returns: J in matrix form (N, d).
-        """
-        return F.sigmoid(x).mul(1 - F.sigmoid(x))
-
-    def h_grad_sigmoid(self, x):
-        """
-        :param x: (N, d)
-        :returns: J in matrix form (N, d).
-        """
-        raise NotImplementedError
-        # return x > 0
-
+    @staticmethod
     def zero_grad_tensor(self, t):
-        # TODO: t.grad.detach_()
+        t.grad.detach_()
         t.grad.zero_()
 
     def J_phi(self, L):